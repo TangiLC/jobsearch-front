03701093a853da08b5fec2edc69408e49b319b07
TangiLC <tangi@le-cadre.net>
2024-08-30 21:46:06 +0200

    redux

---
+++ src/jobSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchJobs = createAsyncThunk('jobs/fetchJobs', async () => {
+  const response = await axios.get('http://localhost:3003/api/get_jobs');
+  console.log(response.data)
+  return response.data;
+});
+
+const jobSlice = createSlice({
+  name: 'jobs',
+  initialState: {
+    loading: false,
+    jobs: [],
+    error: '',
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchJobs.pending, (state) => {
+        state.loading = true;
+        state.error = '';
+      })
+      .addCase(fetchJobs.fulfilled, (state, action) => {
+        state.loading = false;
+        state.jobs = action.payload;
+      })
+      .addCase(fetchJobs.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default jobSlice.reducer;
